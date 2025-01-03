5f25a1b6f20766957989504c0a5234b4feacc2a3
Rahwbahwdawhb <119530761+Rahwbahwdawhb@users.noreply.github.com>
2025-01-03 19:16:19 +0100

    Added terminal window part to GUI

--- gui_terminal.py
+++ gui_terminal.py
@@ -1,7 +1,8 @@
-from PyQt6.QtWidgets import QApplication, QWidget, QHBoxLayout, QVBoxLayout, QGridLayout, QSizePolicy, QLineEdit, QLabel, QPlainTextEdit
-from PyQt6.QtGui import QColor, QFont
+from PyQt6.QtWidgets import QApplication, QWidget, QHBoxLayout, QVBoxLayout, QGridLayout, QSizePolicy, QLineEdit, QLabel, QPlainTextEdit, QTextEdit,QListWidget,QListWidgetItem, QScrollArea
+from PyQt6.QtGui import QColor, QFont,QTextCursor,QFontDatabase
 from PyQt6.QtCore import Qt
 from PyQt6 import QtWidgets
+
 
 
 class PlainTextEdit(QPlainTextEdit):
@@ -49,20 +50,47 @@ class PlainTextEdit(QPlainTextEdit):
         cursor=self.textCursor()
         cursor.setPosition(len(new_prompt))
         self.setTextCursor(cursor)
+
+
+
 class GUI(QWidget):
     def __init__(self):
         super().__init__()
+        self.resize(800,400)
         main_layout=QVBoxLayout()
-        self.le=QLineEdit()
-        main_layout.addWidget(self.le)
+
+        self.terminal_window=QLabel()
+        self.terminal_window.setTextInteractionFlags(Qt.TextInteractionFlag.TextSelectableByMouse)
+        self.terminal_window.setAlignment(Qt.AlignmentFlag.AlignBottom)
+        self.terminal_window.setStyleSheet("QLabel { background-color : black; color : white;}")
+        self.terminal_scroll=QScrollArea()
+        self.terminal_scroll.setWidget(self.terminal_window)
+        self.terminal_scroll.setWidgetResizable(True)
+        
+        main_layout.addWidget(self.terminal_scroll)
+        main_layout.setContentsMargins(0,0,0,0)
 
         self.te=PlainTextEdit()
         main_layout.addWidget(self.te)
         self.setLayout(main_layout)
-
+        
+        self.le=QLineEdit()
         self.le.returnPressed.connect(self.update)
+        main_layout.addWidget(self.le)
+    def add_text_to_terminal(self,text):
+        current_prompt=self.terminal_window.text()
+        if current_prompt:
+            new_prompt=f"{current_prompt}\n\n{text}"
+        else:
+            new_prompt=text
+        self.terminal_window.setText(new_prompt)
+        self.terminal_scroll.verticalScrollBar().setValue(self.terminal_scroll.verticalScrollBar().maximum()+10)
+        self.terminal_scroll.verticalScrollBar().rangeChanged.connect(self.scroll_to_bottom)
+    
+    def scroll_to_bottom(self):
+        self.terminal_scroll.verticalScrollBar().setValue(self.terminal_scroll.verticalScrollBar().maximum())
     def update(self):
-        print(self.le.text())
+        self.add_text_to_terminal(self.le.text())
 
 app=QApplication([])
 app.setFont(QFont('Times New Roman'))
